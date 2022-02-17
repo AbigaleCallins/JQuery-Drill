2c0c8253c775732154f0a80e341474aeab84942e
AbigaleCallins <alcallins@gmail.com>
2022-02-17 09:42:27 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function () {
+    // console.log("test")
+    $('#btnSubmit').click(function () {
+        if ($("input[type=text]").val == ("")) {
+           
+        }
+        alert($("input[type=text]").val())
+    });
+
+});
